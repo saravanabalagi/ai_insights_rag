af32fb04de9d02670807122b02d33fab2d72fc01
Saravanabalagi Ramachandran <saravanabalagi@hotmail.com>
2024-10-21 00:58:23 +0100

    Update port config

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/api",
+        target: "http://localhost:8000/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
